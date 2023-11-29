b71d5c05374cf92c168ced124c9b2ea9714225e3
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-11-29 19:11:50 +0200

    wip: error with loading resources for adminjs/nestjs

--- src/root-module/app.module.ts
+++ src/root-module/app.module.ts
@@ -7,6 +7,26 @@ import Joi from 'joi';
 import { MongooseModule } from '@nestjs/mongoose';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionsFilter } from '../filters/all-exceptions.filter.js';
+import * as AdminJSMongoose from '@adminjs/mongoose';
+import AdminJS from 'adminjs';
+import { TodoMongooseModel } from '../todos/entities/todo.entity.js';
+
+AdminJS.registerAdapter({
+  Resource: AdminJSMongoose.Resource,
+  Database: AdminJSMongoose.Database,
+});
+
+const DEFAULT_ADMIN = {
+  email: 'admin@example.com',
+  password: 'password',
+};
+
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
 
 @Module({
   imports: [
@@ -43,6 +63,27 @@ import { AllExceptionsFilter } from '../filters/all-exceptions.filter.js';
       },
       inject: [ConfigService],
     }),
+
+    import('@adminjs/nestjs').then(({ AdminModule }) =>
+      AdminModule.createAdminAsync({
+        useFactory: () => ({
+          adminJsOptions: {
+            rootPath: '/admin',
+            resources: [TodoMongooseModel],
+          },
+          auth: {
+            authenticate,
+            cookieName: 'adminjs',
+            cookiePassword: 'secret',
+          },
+          sessionOptions: {
+            resave: true,
+            saveUninitialized: true,
+            secret: 'secret',
+          },
+        }),
+      }),
+    ),
     TodosModule,
   ],
   controllers: [AppController],
--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -1,6 +1,6 @@
 import { BaseEntity } from './../../entities/base-entities.js';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, model, Schema as MongooseSchema } from 'mongoose';
 
 @Schema({ timestamps: true, versionKey: false })
 export class Todo extends BaseEntity {
@@ -17,3 +17,14 @@ export class Todo extends BaseEntity {
 export type TodoDocument = Todo & Document;
 
 export const TodoSchema = SchemaFactory.createForClass(Todo);
+
+export const TodoMongooseSchema = new MongooseSchema<Todo>(
+  {
+    name: { type: 'String', required: true },
+    description: { type: 'String', required: true },
+    completed: { type: 'Boolean', required: true },
+  },
+  { timestamps: true },
+);
+
+export const TodoMongooseModel = model<Todo>('Todo', TodoSchema, 'todos');
